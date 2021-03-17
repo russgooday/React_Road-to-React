d2c24df16feef745c5d189f47cad1c48dd47bd19
Russell Gooday <rpg_digital@yahoo.co.uk>
2021-03-17 00:42:55 +0000

    update readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Frameworks</h1>
+      {
+        list.map(
+          ({ title, author, objectID }) => (
+            <div key={objectID}>
+              <h2>{title}</h2>
+              <p>author: {author}</p>
+            </div>
+          )
+        )
+      }
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' />
     </div>
   );
 }
